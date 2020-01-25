fdab8719e95c12d5afef92498939ead8fcb14c34
petitlatte <lisasumida@gmail.com>
2020-01-25 22:56:57 +1300

    button working

--- Current Projects/Project 1/Basic Website/js/script.js
+++ Current Projects/Project 1/Basic Website/js/script.js
@@ -1,7 +1,9 @@
 (function() {
 
-function DonateFunction(){
-
-};
+var message = document.getElementById("test");
+var message2 = document.getElementById("test2");
+document.getElementById("donatebutton").addEventListener("click", function() {
+    alert( "Thank you " + message.value + " ! " + " You have donated $" + message2.value + " to Kakapo Recovery! ");
+});
 
 })();
